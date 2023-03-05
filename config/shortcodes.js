e35836b16a81c38d998f2110cd307dfaf3803e14
Paul <paul@theapplegates.com>
2023-03-05 18:59:28 -0500

    Update shortcodes.js

--- config/shortcodes.js
+++ config/shortcodes.js
@@ -1,12 +1,12 @@
 const Image = require('@11ty/eleventy-img')
 
 const imageShortcode = async (imageObj = {}) => {
-  const widths = imageObj.widths || [300, 600, 900, 1200]
+  const widths = imageObj.widths || [100, 300, 600, 900, 1200, 2400]
   const className = imageObj.className || "image"
 
   const sizes = "(min-width: 100px) 50vw, 100vw"
   const metadata =  await Image(imageObj.src, {
-      formats: ["webp"],
+      formats: ["avif", "webp"],
       outputDir: "./_site/assets/images/generated/",
       urlPath: "/assets/images/generated/",
       widths: widths
